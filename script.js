6a681f79982fbb121ece3ac574e4500b43a026e8
ayatsakalla <525asakalla@frhsd.com>
2024-10-16 17:31:00 -0400

    added halloween font

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
+
+    // click on the button under the ghost home page
     $('#beware').on('click', () => {
         $('#beware').hide();
         $('#boo-text').text('BOO!');
         $('#cont-fluid').removeClass('d-none').fadeIn();
     })
+
+
+    
 });
