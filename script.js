c7b9251f7f6916ca57906fa1405f0e0ccb7a6dfe
ayatsakalla <525asakalla@frhsd.com>
2024-10-16 18:42:03 -0400

    added another font, google maps iframe, and intro to my event info before i begin jq stuff

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ $(document).ready(function () {
     // click on the button under the ghost home page
     $('#beware').on('click', () => {
         $('#beware').hide();
+        $('#boo-text').removeClass('magnificent');
+        $('#boo-text').addClass('feast');
+        $('#boo-text').addClass('display-1');
         $('#boo-text').text('BOO!');
         $('#cont-fluid').removeClass('d-none').fadeIn();
     })
 
 
-    
+
 });
